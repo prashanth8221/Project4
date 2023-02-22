c53f7a428f2e50668150793ab9e1e8abe938eca3
prashanth8221 <prashanthpro777@gmail.com>
2023-02-22 15:06:37 +0530

    inital commit

---
+++ calc.py
@@ -0,0 +1,29 @@
+#def is_even(x):
+ #   return x % 2 == 0
+nums = [2,3,45,5,66,77,6,3,32,334]
+even = list(filter(lambda a: a % 2 == 0 , nums))
+print(even)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''add = lambda a, b : a+b
+mul = lambda a, b : a*b
+div = lambda a, b : a/b
+
+result = div(36,6)
+print(result)'''
